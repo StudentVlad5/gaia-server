e94b73797e455f06049130fc114e251b63b26b50
Vladyslav Popov <vlad_np@ukr.net>
2026-04-21 20:44:58 +0200

    renovate role for ADMIN

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,9 +3,9 @@ const controller = require("../controllers/users.controller");
 const auth = require("../middleware/auth.middleware");
 const role = require("../middleware/role.middleware");
 
-router.get("/", auth, controller.getUsers);
-// router.get("/", auth, role("ADMIN"), controller.getUsers);
-router.put("/:id/role", auth, controller.changeRole);
-// router.put("/:id/role", auth, role("ADMIN"), controller.changeRole);
+// router.get("/", auth, controller.getUsers);
+router.get("/", auth, role("ADMIN"), controller.getUsers);
+// router.put("/:id/role", auth, controller.changeRole);
+router.put("/:id/role", auth, role("ADMIN"), controller.changeRole);
 
 module.exports = router;
