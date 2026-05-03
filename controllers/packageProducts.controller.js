19e7eff96b64a4b5e751f7fc9a69e3a87dd75d3c
Vladyslav Popov <vlad_np@ukr.net>
2026-05-03 09:40:57 +0200

    fix response for product

--- controllers/packageProducts.controller.js
+++ controllers/packageProducts.controller.js
@@ -2,17 +2,26 @@ const service = require("../services/packageProducts.service");
 
 exports.getAll = async (req, res) => {
   const data = await service.getAll();
-  res.json(data);
+  res.json({
+    success: true,
+    data,
+  });
 };
 
 exports.create = async (req, res) => {
   const data = await service.create(req.body);
-  res.json(data);
+  res.json({
+    success: true,
+    data,
+  });
 };
 
 exports.update = async (req, res) => {
   const data = await service.update(req.params.id, req.body);
-  res.json(data);
+  res.json({
+    success: true,
+    data,
+  });
 };
 
 exports.remove = async (req, res) => {
